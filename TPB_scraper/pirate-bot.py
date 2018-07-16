50164c983244961039a7987425349cf31982c051
SpaceBoo <jtc72374@uga.edu>
2018-07-16 15:47:34 -0400

    Tuning

--- TPB_scraper/pirate-bot.py
+++ TPB_scraper/pirate-bot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-# pirate-bot
-# https://github.com/SpaceBoo/TPB-scraper
-# piratebot --help
+"""
+pirate-bot
+https://github.com/SpaceBoo/TPB-scraper
+piratebot --help
+"""
 
 import json
 import argparse
